f5bed802cb74a5181d85141a7a49c050ebac36be
fabroo <fcorzini@kovix-group.com>
2024-04-11 10:22:12 -0300

    tp 3 hecho

--- TPs/tp2.cpp
+++ TPs/tp2.cpp
@@ -1,41 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <limits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> lis;
-int best_increasing_sum = 0;
-
-vector<int> longestIncreasingSequence(int i, int last, vector<int> &temp, vector<int> h, vector<int> w, vector<int> &M)
-{
-    if (i == h.size())
-    {
-        int sum = accumulate(temp.begin(), temp.end(), 0);
-        if (temp.size() >= lis.size() && sum > best_increasing_sum)
-        {
-            lis = temp;
-            best_increasing_sum = sum;
-        }
-        return lis;
-
-        if (h[i] >= last)
-        {
-            if (M[i + 1] == numeric_limits<int>::min())
-            {
-                vector<int> newVec = temp;
-                newVec.push_back(w[i]);
-
-                
-                M[i + 1] = 
-            }
-        }
-    }
-}
 int main()
 {
-    vector<int> h = {1, 2, 3};
-    cout << "cant tests \n";
+    int t, n;
+    scanf("%d", &t);
+    for (int k = 1; k <= t; k++)
+    {
+        scanf("%d", &n);
+        int h[n], w[n], lis[n], lds[n];
+
+        // completo las heights y widths
+        for (int i = 0; i < n; i++)
+            scanf("%d", &h[i]);
+        for (int i = 0; i < n; i++)
+            scanf("%d", &w[i]);
+
+        // vamos a calcular la LIS que termina en i (subproblema optimo)
+        for (int i = 0; i < n; i++)
+        {
+            // sabemos que el minimo valor mas grande para cada i, es el width del edificio i solo
+            lis[i] = w[i];
+            for (int j = 0; j < i; j++)
+            {
+                if (h[i] > h[j])
+                    lis[i] = max(lis[i], lis[j] + w[i]);
+            }
+        }
+
+        // vamos a calcular la LDS que termina en i (subproblema optimo)
+        for (int i = 0; i < n; i++)
+        {
+            // sabemos que el minimo valor mas grande para cada i, es el width del edificio i solo
+            lds[i] = w[i];
+            for (int j = 0; j < i; j++)
+            {
+                if (h[i] < h[j])
+                    lds[i] = max(lds[i], lds[j] + w[i]);
+            }
+        }
+
+        int suma_creciente = 0;
+        int suma_decreciente = 0;
+        // tanto para LIS como LDS buscamos cuales son la maxima suma que obtenemos = maxima subsec.
+        for (int i = 0; i < n; i++)
+        {
+            suma_creciente = max(suma_creciente, lis[i]);
+            suma_decreciente = max(suma_decreciente, lds[i]);
+        }
+        if (suma_creciente >= suma_decreciente)
+        {
+            printf("Case %d. Increasing (%d). Decreasing (%d).\n", k, suma_creciente, suma_decreciente);
+        }
+        else
+        {
+            printf("Case %d. Decreasing (%d). Increasing (%d).\n", k, suma_decreciente, suma_creciente);
+        }
+    }
     return 0;
 }
